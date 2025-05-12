102a6f7065fafbd4da56dc1112e2f2cf5a2280d3
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-12 22:26:06 +0100

    Update Clap

--- resources/views/components/follow-ctr.blade.php
+++ resources/views/components/follow-ctr.blade.php
@@ -1,13 +1,12 @@
 @props(['user' => null])
 
-<div {{  $attributes }} x-data="{
-    following: {{ $user && auth()->user() ? $user->isFollowedBy(auth()->user()) ? 'true' : 'false' : 'false' }},
+<div {{ $attributes }} x-data="{
+    following: {{ $user && auth()->user() ? ($user->isFollowedBy(auth()->user()) ? 'true' : 'false') : 'false' }},
     followersCount: {{ $user ? $user->followers()->count() : 0 }},
     follow() {
-        this.following = !this.following
         axios.post('/follow/{{ $user ? $user->id : 0 }}')
             .then(res => {
-                console.log(res.data)
+                this.following = !this.following
                 this.followersCount = res.data.followersCount;
             })
             .catch(err => {
@@ -16,5 +15,5 @@
 
     }
 }" class="w-[320px] border-l px-8">
-{{ $slot }}
+    {{ $slot }}
 </div>
