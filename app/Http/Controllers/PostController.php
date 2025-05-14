7aea185ddbaaaab72f66937a528403b7b729379a
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-14 11:43:23 +0100

    Update media

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,15 +19,18 @@ class PostController extends Controller
     {
         $user = auth()->user();
 
-        $query = Post::latest();
-        if($user){
+        $query = Post::with(['user', 'media'])
+            ->withCount('claps')
+            ->latest();
+
+        if ($user) {
             $ids = $user->following()->pluck('users.id');
             $query->whereIn('user_id', $ids);
         }
 
         $posts = $query->simplepaginate(5);
         return view('post.index', [
-           'posts'=>$posts,
+            'posts' => $posts,
         ]);
     }
 
@@ -38,7 +41,7 @@ class PostController extends Controller
     {
         $categories = Category::get();
         return view('post.create', [
-            'categories' =>$categories,
+            'categories' => $categories,
         ]);
     }
 
@@ -61,7 +64,7 @@ class PostController extends Controller
         $post = Post::create($data);
 
         $post->addMediaFromRequest('image')
-        ->toMediaCollection();
+            ->toMediaCollection();
 
         return redirect()->route('dashboard');
     }
@@ -69,10 +72,10 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $username,Post $post)
+    public function show(string $username, Post $post)
     {
-        return view ('post.show', [
-            'post'=> $post,
+        return view('post.show', [
+            'post' => $post,
 
         ]);
     }
@@ -104,7 +107,11 @@ class PostController extends Controller
 
     public function category(Category $category)
     {
-        $post = $category->posts()->latest()->simplePaginate(5);
+        $post = $category->posts()
+            ->with(['user', 'media'])
+            ->withCount('claps')
+            ->latest()
+            ->simplePaginate(5);
 
         return view('post.index', [
             'posts' => $post,
